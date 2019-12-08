929ca3a67a457cc0d27f555fa88945937b0eb6c0
HyungjunAn <hyungjun0429@gmail.com>
2019-12-08 21:51:27 +0900

    mod vim batch: check admin, vimrc path auto generate

--- vim/GvimSettingFilesMoving.bat
+++ vim/GvimSettingFilesMoving.bat
@@ -1,12 +1,20 @@
 @echo off
-title AD's Gvim Setting Files Moving
-rem -------------------------------------------------------
-rem Author	: An, Hyungjun
-rem E-mal	: hyungjun@pusan.ac.kr
-rem -------------------------------------------------------
-rem -------------------------------------------------------
-rem Step 0. 관리자 권한 확인
-
+echo ------------------------------------------------------
+echo 	Title	: AD's Gvim Setting Files Moving
+echo 	Author	: An, Hyungjun
+echo 	E-mal	: hyungjun0429@gmail.com
+echo ------------------------------------------------------
+echo.
+echo ------------------------------------------------------
+echo 	Check Admin
+echo ------------------------------------------------------
+fsutil dirty query %systemdrive% > nul
+if '%errorlevel%' NEQ '0' (		
+    echo !!!      Please Run as Administrator      !!!
+    goto EXIT
+)
+pushd "%~dp0"
+echo.
 echo ------------------------------------------------------
 echo 	Gvim 테마, vimrc, vim 파일 이동
 echo ------------------------------------------------------
@@ -16,11 +24,14 @@ copy "syntax"	"%VIMPATH%\syntax\"
 
 copy ctags58.exe 				"%VIMPATH%\ctags.exe"
 
-copy source_cmd.vim				"%USERPROFILE%\_vimrc"
-
+echo source %cd%\vimrc_AD.vim > "%USERPROFILE%\_vimrc"
+echo.
+echo ------------------------------------------------------
+echo 	Vundle 다운로드
+echo ------------------------------------------------------
 git clone https://github.com/VundleVim/Vundle.vim.git %USERPROFILE%/vimfiles/
-
+echo.
 echo ------------------------------------------------------
 echo (종료를 원하시면 아무 키나 누르세요...)
-echo ------------------------------------------------------
+:EXIT
 pause > nul
--- vim/source_cmd.vim
+++
@@ -1,2 +0,0 @@
-"source ~/<path>/vim/vimrc_AD.vim
-source ~\Google\ 드라이브\pc_setting\vim\vimrc_AD.vim
